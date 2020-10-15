dc3599e5f1180dcd180d909a2cfe6f5708a790c1
Genilson <genilson.nascimento@coteminas.com.br>
2020-10-14 21:58:48 -0300

    :tada: Inicianando projeto

---
+++ src/service/sellProduct.js
@@ -0,0 +1,13 @@
+/**
+ * A quantidade vendida pode ser  de 1 ou mais unidades
+ * --> Se estoque ficar negativo uma exception deve ser lançada
+ * -->  O vaor de venda não pode ser maior do que o valor de compra
+ * @param {*} product 
+ * @param {*} ammout 
+ */
+
+export default function sellProduct(product, ammout) {
+
+  product.stock -= 1
+  return product
+}
